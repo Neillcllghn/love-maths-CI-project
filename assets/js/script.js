0ec8d7e1444d103d9ab2bdff7b1ef3542faf7113
Neillcllghn <neillcllghn@outlook.com>
2022-10-22 10:02:32 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,12 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if(event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
     
 })
@@ -26,6 +32,11 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    //this will automatically set cursor to answer box
+    document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 + 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
